bf6b125f0a66385281f57036896eb11fd8631fd2
H-EH7 <fly3767@gmail.com>
2023-06-11 15:23:12 +0900

    Spring DB 1편 - JDBC 이해 - JDBC 개발 - 등록
    - 리소스 정리
    예외가 발생할 경우를 생각해 꼭 finally 구문에서 커넥션을 끊어주어야 함
    
    * SQL Injection 공격을 예방하려면 Statement의 자식 타입인 PreparedStatement를 통해서 파라미터를 바인딩 해주어야 함

---
+++ Spring 강의/스프링 DB 1편/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,66 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql); // 쿼리 등록
+            pstmt.setString(1, member.getMemberId()); // ?에 String 바인딩
+            pstmt.setInt(2, member.getMoney()); // ?에 int 바인딩
+            pstmt.executeUpdate(); // 쿼리 실행, 영향 받은 row 수 만큼 값 반환됨
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
